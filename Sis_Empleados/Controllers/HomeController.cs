24d38963a6552a600490b8064106def614d7f61d
sackz23 <sergio84756@gmail.com>
2025-11-10 22:21:30 -0600

    agregando dashboard y login

--- Sis_Empleados/Controllers/HomeController.cs
+++ Sis_Empleados/Controllers/HomeController.cs
@@ -6,20 +6,49 @@ namespace Sis_Empleados.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            // Si no hay sesión activa, redirige al login
+            if (HttpContext.Session.GetInt32("UsuarioId") == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
+            var usuarioId = HttpContext.Session.GetInt32("UsuarioId");
+            var usuario = _context.Usuarios
+                .Where(u => u.Id_Usuario == usuarioId)
+                .Select(u => new { u.Nombre_Usuario, RolNombre = u.Rol.Nombre_Rol })
+                .FirstOrDefault();
+
+            if (usuario != null)
+            {
+                ViewBag.Usuario = usuario.Nombre_Usuario;
+                ViewBag.Rol = usuario.RolNombre;
+            }
+
+            // Si ya está logueado, muestra la página principal
             return View();
         }
 
         public IActionResult Privacy()
         {
+            // Verifica si hay sesión activa
+            if (HttpContext.Session.GetInt32("UsuarioId") == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
+            // Obtiene los datos de sesión
+            ViewBag.Usuario = HttpContext.Session.GetString("NombreUsuario");
+            ViewBag.RolId = HttpContext.Session.GetInt32("RolId");
+
             return View();
         }
 
--- Sis_Empleados/obj/Debug/net8.0/Sis_Empleados.AssemblyInfo.cs
+++ Sis_Empleados/obj/Debug/net8.0/Sis_Empleados.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sis_Empleados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2008041562e0790c2ae98e694c946f6837f7c223")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bbc83326219830b19b7d4af30fa3564671975e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sis_Empleados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sis_Empleados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
